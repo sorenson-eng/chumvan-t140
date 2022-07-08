a2bce9f88c1977c78ae2a38ff906f1651f6ac1d8
Trung Van <trung.van@ericsson.com>
2022-07-08 15:26:16 +0300

    add redundancy handling

--- t140packet/error.go
+++ t140packet/error.go
@@ -3,7 +3,10 @@ package t140packet
 import "errors"
 
 var (
-	errNoExtensionAllowed = errors.New("no extension is allowed in T140 packet")
-	errNoCSRCAllowed      = errors.New("CC must be 0 and CSRC must be nil in T140 packet")
-	errTooLargePayload    = errors.New("payload is too large for T140 packet, keep it short")
+	errNoExtensionAllowed        = errors.New("no extension is allowed in T140 packet")
+	errNoCSRCAllowed             = errors.New("CC must be 0 and CSRC must be nil in T140 packet")
+	errTooLargePayload           = errors.New("payload is too large for T140 packet, keep it short")
+	errInvalidREDHeader          = errors.New("redundant block header must start with bit 1")
+	errMismatchREDHeaderAndBlock = errors.New("mismatch in number of RED header and data block")
+	errMismatchRHeaderSize       = errors.New("redundant block header size must be 4 bytes")
 )
--- t140packet/t140packet.go
+++ t140packet/t140packet.go
@@ -2,24 +2,47 @@
 package t140packet
 
 import (
+	"encoding/binary"
+
 	"github.com/pion/rtp"
 )
 
-const payloadMaxSize uint16 = 512
+const (
+	payloadMaxSize = 512
+	rHeaderSize    = 4
+	rHeaderMask    = 0x08
+	ptMask         = 0x7F
+
+	timeOffsetOffset = 1
+	timeOffsetShift  = 2
+	timeOffsetMask   = 0x3FFF
+	timeOffsetSize   = 2 // 14 bit
+)
 
 // T140Packet represents a T140 packet as a form of RTP packet.
 // Header is an RTP header without extensions or CSRCs.
 // Payload contains redundant data if RED-flag is true.
 type T140Packet struct {
-	Header  rtp.Header
-	Payload []byte
-	IsRED   bool
+	Header rtp.Header
+	IsRED  bool
+
+	// RED
+	RHeader []RBlockHeader
+
+	PBlock []byte
+	RBlock []byte
+}
+
+type RBlockHeader struct {
+	PayloadType     uint8
+	TimestampOffset uint16
+	BlockLength     uint16
 }
 
 // Unmarshal parses the passed in byte slice
-// and store the result in the T140Packet this method is called upon.
+// and stores the result in the T140Packet this method is called upon.
 // Returns any occurred error
-func (t *T140Packet) Unmarshal(buf []byte, codeRED int8) (err error) {
+func (t *T140Packet) Unmarshal(buf []byte, codeRED uint8) (pBlock []byte, rBlock []byte, err error) {
 	rtpPacket := &rtp.Packet{}
 	err = rtpPacket.Unmarshal(buf)
 	if err != nil {
@@ -28,19 +51,84 @@ func (t *T140Packet) Unmarshal(buf []byte, codeRED int8) (err error) {
 	if rtpPacket.Header.Extension ||
 		rtpPacket.Header.Extensions != nil ||
 		rtpPacket.Header.ExtensionProfile != 0 {
-		return errNoExtensionAllowed
+		err = errNoExtensionAllowed
+		return
 	}
 	if rtpPacket.Header.CSRC != nil {
-		return errNoCSRCAllowed
+		err = errNoCSRCAllowed
+		return
 	}
 	if len(rtpPacket.Payload) > int(payloadMaxSize) {
-		return errTooLargePayload
+		err = errTooLargePayload
+		return
 	}
 
+	// If redundancy is applied
 	t.Header = rtpPacket.Header
-	t.Payload = rtpPacket.Payload
 	if t.Header.PayloadType == uint8(codeRED) {
 		t.IsRED = true
 	}
+
+	err = t.UnmarshalBlock(rtpPacket.Payload)
+	if err != nil {
+		return
+	}
+
 	return
+}
+
+// UnmarshalBlock parses the passed in byte slice
+// and stores the block(s) in the T140Packet this method is called upon.
+// Returns any occurred error.
+func (t *T140Packet) UnmarshalBlock(payload []byte) (err error) {
+	// Payload of T140 packet can be empty
+	if len(payload) == 0 {
+		return
+	}
+
+	if !t.IsRED {
+		t.PBlock = payload
+		return
+	}
+
+	rCount, err := CountREDHeaders(payload)
+	if err != nil {
+		return
+	}
+
+	for i := 0; i <= rCount; i++ {
+		payload[i*rHeaderSize]
+	}
+
+	return
+}
+
+// CountREDHeaders checks and counts the number of RED headers in the passed in byte slice.
+// Returns number of RED headers and any occurred error.
+func CountREDHeaders(payload []byte) (count int, err error) {
+	if payload[0]&rHeaderMask == 0 {
+		err = errInvalidREDHeader
+		return
+	}
+
+	rowCount := len(payload) / rHeaderSize
+	for i := 0; i <= rowCount; i++ {
+		if payload[i*rHeaderSize]&rHeaderMask == 1 {
+			count++
+		}
+	}
+
+	return
+}
+
+func (t *T140Packet) UnmarshalRHeader(buf []byte) (err error) {
+	if len(buf) != 4 {
+		return errMismatchRHeaderSize
+	}
+	if buf[0]&rHeaderMask == 0 {
+		return errInvalidREDHeader
+	}
+	rHeader := &RBlockHeader{}
+	rHeader.PayloadType = buf[0] & ptMask
+	rHeader.TimestampOffset = binary.BigEndian.Uint16(buf[timeOffsetOffset : timeOffsetOffset+tim])
 }
