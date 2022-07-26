a8192b9fae42862a35a54c3aa1286181f5b4acb3
Trung Van <trung.van@ericsson.com>
2022-07-26 13:52:00 +0300

    edit string

--- t140packet/t140packet.go
+++ t140packet/t140packet.go
@@ -207,8 +207,8 @@ func (t T140Packet) String() string {
 	s += fmt.Sprintf("\tIs RED: %t\n", t.IsRED)
 	s += fmt.Sprintf("\tP-block length: %d bytes\n", len(t.PBlock))
 	s += fmt.Sprintf("\tR-blocks quantity: %d\n", len(t.RBlocks))
-	s += fmt.Sprintf("\tR-blocks: %#v\n", t.RBlocks)
-	s += fmt.Sprintf("\tPayload: %#v\n", t.Payload)
+	s += fmt.Sprintf("\tR-blocks: %v\n", t.RBlocks)
+	s += fmt.Sprintf("\tPayload: %v\n", t.Payload)
 	return s
 }
 
